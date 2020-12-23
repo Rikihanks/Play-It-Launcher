6d9090427f82adb62e6ecc95c22343400bb4b7ee
rikihanks <richard_hanks@outlook.com>
2020-12-23 15:52:05 +0000

    login animation frame for profile picture

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,11 +6,34 @@ import { IgdbService } from 'src/app/services/igdb.service';
 import { IpcService } from 'src/app/services/ipc.service';
 import { LoaderService } from 'src/app/services/loader.service';
 import { Game } from 'src/app/models/Game';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.less']
+  styleUrls: ['./main.component.less'],animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        transition(
+          ':enter', 
+          [
+            style({  opacity: 0, boxShadow: 'none', offset: 0 }),
+            animate('2s ease-out', 
+                    style({ opacity: 1 , boxShadow: '0 0 35px #39ceb0', offset: 0.1}))
+          ]
+        ),
+        transition(
+          ':leave', 
+          [
+            style({ height: 300, opacity: 1 }),
+            animate('1s ease-in', 
+                    style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class MainComponent implements OnInit, OnDestroy {
 
